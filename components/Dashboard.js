70cca22155d23b521ac0c1f962c6af1d474e7421
Dali Duraku <mesherbetmolla@gmail.com>
2025-03-10 11:55:42 -0400

    feat: round average mood number to two decimal places

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -29,7 +29,7 @@ export default function Dashboard() {
     }
     return {
       num_days: total_number_of_days,
-      average_mood: sum_moods / total_number_of_days,
+      average_mood: (sum_moods / total_number_of_days).toFixed(2),
     };
   }
 
